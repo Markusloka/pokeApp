108a471a5e1e82f5412ac09234834538fd14b754
Markus Lokander <Markuslokander97@gmail.com>
2024-12-14 18:18:06 +0100

    added search button
    search button added

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <body>
+    <div class="search"><SearchBar /></div>
+  </body>
+</template>
+
+<style>
+body {
+  height: 100%; /* Make sure html and body span the full height of the page */
+  margin: 0; /* Remove default margin */
+  padding: 0; /* Remove default padding */
+  background-color: #000000; /* Pitch-black background */
+  color: #ffffff; /* White text for contrast */
+  font-family: Arial, sans-serif; /* Optional: Set a clean font */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  justify-items: center;
+}
+.search {
+}
+</style>
